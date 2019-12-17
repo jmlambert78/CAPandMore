d194959b8a73483ae414ff7315f7b567b33f3fc3
jmlambert78 <jmlambert78@gmail.com>
2019-12-17 14:14:09 +0100

    for azure after tfcaasp4

---
+++ CAP-AZURE-171219/.envvar.sh
@@ -0,0 +1,111 @@
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export CAP_VERSION="1.5.1"
+export UAA_HELM_VERSION="suse/uaa --version 2.19.1 "
+export SCF_HELM_VERSION=" suse/cf --version 2.19.1 "
+export CONSOLE_HELM_VERSION=" --version 2.6.1 "
+export METRICS_HELM_VERSION=" --version 1.1.1 "
+export NEXT_UPGRADE_PATH="1.5.2"
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SBRGNAME=1KoqEshh-service-broker
+export REGION=westeurope
+export SERVICE_PRINCIPAL_INFO='{
+  "appId": "e38fcbd9-0f54-439d-aefe-bd174ed86a19",
+  "displayName": "1KoqEshh-service-broker",
+  "name": "http://1KoqEshh-service-broker",
+  "password": "4eb25787-505b-49ad-ad94-6a25db44a031",
+  "tenant": "856b813c-16e5-49a5-85ec-6f081e13b527"
+}'
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SBRGNAME=m4x8fcdP-service-broker
+export REGION=westeurope
+export SERVICE_PRINCIPAL_INFO='{
+  "appId": "39abef7b-2f11-48b1-a350-79653fb353ec",
+  "displayName": "m4x8fcdP-service-broker",
+  "name": "http://m4x8fcdP-service-broker",
+  "password": "ae8e31a0-cd39-49bf-8ae5-3606ebcc0ed2",
+  "tenant": "856b813c-16e5-49a5-85ec-6f081e13b527"
+}'
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SBRGNAME=M8YttESg-service-broker
+export REGION=westeurope
+export SERVICE_PRINCIPAL_INFO='{
+  "appId": "88b4cc19-03c1-45ec-b11c-311a0aea9df9",
+  "displayName": "M8YttESg-service-broker",
+  "name": "http://M8YttESg-service-broker",
+  "password": "029a8759-0efa-418a-a518-d5c33b21edc5",
+  "tenant": "856b813c-16e5-49a5-85ec-6f081e13b527"
+}'
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SBRGNAME=xyHWduni-service-broker
+export REGION=westeurope
+export SERVICE_PRINCIPAL_INFO='{
+  "appId": "4a2c9679-3a51-452f-aafb-a3c93fc807c2",
+  "displayName": "xyHWduni-service-broker",
+  "name": "http://xyHWduni-service-broker",
+  "password": "d309c23b-fdb3-4b0e-87ef-422277c7ccb6",
+  "tenant": "856b813c-16e5-49a5-85ec-6f081e13b527"
+}'
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SBRGNAME=TaKMaYm4-service-broker
+export REGION=westeurope
+export SERVICE_PRINCIPAL_INFO='{
+  "appId": "95b7a9bd-4849-4669-8daa-8e89a9f3bd39",
+  "displayName": "TaKMaYm4-service-broker",
+  "name": "http://TaKMaYm4-service-broker",
+  "password": "c87b359a-359b-47c6-8319-ba51e33834a6",
+  "tenant": "856b813c-16e5-49a5-85ec-6f081e13b527"
+}'
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
+export SBRGNAME=BjnuILla-service-broker
+export REGION=westeurope
+export SERVICE_PRINCIPAL_INFO='{
+  "appId": "78cd1c6d-3670-4efd-a2de-aa9382a6f3fa",
+  "displayName": "BjnuILla-service-broker",
+  "name": "http://BjnuILla-service-broker",
+  "password": "38af327c-c0bd-47f3-a44d-c65bfacbed6f",
+  "tenant": "856b813c-16e5-49a5-85ec-6f081e13b527"
+}'
+export CFEP="https://api.cf.private.jmllabsuse.com:443"
+export CFEP="https://api.cf1.jmllabsuse.com:443"
+export CFEP="https://api.cf1.jmllabsuse.com:443"
+export SUBSCRIPTION_ID=1860e2dd-7e67-48e9-97f6-3cbfc8fdfc80
+export KUBECONFIG=/home/jmlambert/capauto/CAP-021219-AKS/kubeconfig
