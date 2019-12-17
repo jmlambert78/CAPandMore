56c7c68a6cbed67c9911bfcea65e1ad2528cfc64
jmlambert78 <jmlambert78@gmail.com>
2019-12-17 14:28:13 +0100

    add azure files examples

---
+++ init171219-azure.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export AKSDEPLOYID="$PWD/CAP-171219-AKS"
+export REGION="westeurope"
+export KUBECONFIG="$AKSDEPLOYID/kubeconfig"
+export CF_HOME="$AKSDEPLOYID/cfconfig"
+export PS1="\u:\w:$AKSDEPLOYID>\[$(tput sgr0)\]"
+
+export PS1="\w:>\[$(tput sgr0)\]"
+
+#CFEP=$(awk '/Public IP:/{print "https://api." $NF ".xip.io"}' $AKSDEPLOYID/deployment.log)
+CFEP="https://api.cf1.jmllabsuse.com:443"
+
+echo "export CFEP=\"$CFEP\"" >>$AKSDEPLOYID/.envvar.sh
+
+cf api --skip-ssl-validation $CFEP
+
